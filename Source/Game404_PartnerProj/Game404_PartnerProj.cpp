05f7f9f7ba91d9107d0504c016c53457e32559f5
mac2648 <60781347+mac2648@users.noreply.github.com>
2024-04-10 14:10:23 -0400

    create base project

---
+++ Source/Game404_PartnerProj/Game404_PartnerProj.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Game404_PartnerProj.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game404_PartnerProj, "Game404_PartnerProj" );
